54cb14553382e13f36c5bed59756d23fee02ab21
strongant <877798942@qq.com>
2016-07-21 00:02:07 +0800

    添加socketio依赖

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var mongoose = require('./config/mongoose'),
   express = require('./config/express'),
   passport = require('./config/passport');
 var db = mongoose();
-var app = express();
+var app = express(db);
 var passport = passport();
 
 app.listen(3000);
